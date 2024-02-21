e766217e6c3b51e20d8601da48e5df45a3e71f27
valentin994 <valentin.vareskic@gmail.com>
2024-02-21 15:42:08 +0100

    added saving all events

--- lib/movement_direction.dart
+++ lib/movement_direction.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'package:path/path.dart';
 
 class AccelStorage {
   Future<String?> get _localPath async {
@@ -15,15 +16,14 @@ class AccelStorage {
    
   Future<File> get _localFile async {
     final path = await _localPath;
-    print(path);
-    return File('$path/accel_data.json');
+    return File('$path/');
   }
 
-  Future<File> writeCounter(Map data) async {
+  Future<File> writeCounter(Map data, String filename) async {
     final file = await _localFile;
-
+    File updatedFile = File(join(file.path, filename));
     // Write the file
-    return file.writeAsString(json.encode(data));
+    return updatedFile.writeAsString(json.encode(data));
   }
 
 }
@@ -38,26 +38,66 @@ class MovementDetection extends StatefulWidget{
 
 class _MovementDetection extends State<MovementDetection>{
   UserAccelerometerEvent? _userAccelerometerEvent;
+  AccelerometerEvent? _accelerometerEvent;
+  GyroscopeEvent? _gyroscopeEvent;
+  MagnetometerEvent? _magnetometerEvent;
+
+
+
   DateTime? _userAccelerometerUpdateTime;
-  int? _userAccelerometerLastInterval;
+  DateTime? _accelerometerUpdateTime;
+  DateTime? _gyroscopeUpdateTime;
+  DateTime? _magnetometerUpdateTime;
+
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
-  String userX = "";
+  AccelStorage storage = AccelStorage();  
+
+  int? _userAccelerometerLastInterval;
+  int? _accelerometerLastInterval;
+  int? _gyroscopeLastInterval;
+  int? _magnetometerLastInterval;
+
+
+
+  String running = "no";
   Duration duration = const Duration(seconds: 5);
   String num = '';
   List list = [];
+  Map userAccelMap = {"x": [], "y": [], "z": []};
+  Map accelMap = {"x": [], "y": [], "z": []};
+  Map gyroMap = {"x": [], "y": [], "z": []};
+  Map magnetoMap = {"x": [], "y": [], "z": []};
+
   void showAccel() {
+      final mark = DateTime.timestamp();
       const Duration duration = Duration(seconds: 5);
       DateTime endTime = DateTime.now().add(duration);
-
-      Timer.periodic(Duration(milliseconds: 10), (Timer timer) {
+      running = "yes";
+      Timer.periodic(const Duration(milliseconds: 10), (Timer timer) {
         if (DateTime.now().isBefore(endTime)) {
-          num = _userAccelerometerEvent?.x.toStringAsFixed(3)??'';
-          list.add(num);
-          print('Function running...');
+          userAccelMap["x"].add(_userAccelerometerEvent?.x);
+          userAccelMap["y"].add(_userAccelerometerEvent?.y);
+          userAccelMap["z"].add(_userAccelerometerEvent?.z);
+          
+          accelMap["x"].add(_accelerometerEvent?.x);
+          accelMap["y"].add(_accelerometerEvent?.y);
+          accelMap["z"].add(_accelerometerEvent?.z);
+
+          gyroMap["x"].add(_gyroscopeEvent?.x);
+          gyroMap["y"].add(_gyroscopeEvent?.y);
+          gyroMap["z"].add(_gyroscopeEvent?.z);
+
+          magnetoMap["x"].add(_magnetometerEvent?.x);
+          magnetoMap["y"].add(_magnetometerEvent?.y);
+          magnetoMap["z"].add(_magnetometerEvent?.z);
+
         } else {
           timer.cancel();
-          var listLen = list.length;
-          print('Number of items $listLen');
+          running = "no";
+          storage.writeCounter(userAccelMap, "user_accel_map-$mark.json");
+          storage.writeCounter(accelMap, "accel_map-$mark.json");
+          storage.writeCounter(gyroMap, "gyro_map-$mark.json");
+          storage.writeCounter(magnetoMap, "magneto_map-$mark.json");
         }
       });
     }
@@ -66,8 +106,7 @@ class _MovementDetection extends State<MovementDetection>{
   @override
   Widget build(context) {
       return Column(children: [
-            Text(_userAccelerometerEvent?.x.toStringAsFixed(1) ?? '?'),
-            Text(num),
+            Text("Am i running: $running"),
             OutlinedButton(onPressed: showAccel, child: const Text('Click me senpai'))
         ],
     );
@@ -101,7 +140,7 @@ class _MovementDetection extends State<MovementDetection>{
         },
         onError: (e) {
           showDialog(
-              context: context,
+              context: context as BuildContext,
               builder: (context) {
                 return const AlertDialog(
                   title: Text("Sensor Not Found"),
@@ -109,6 +148,60 @@ class _MovementDetection extends State<MovementDetection>{
                       "It seems that your device doesn't support User Accelerometer Sensor"),
                 );
               });
+        },
+        cancelOnError: true,
+      ),
+    );
+ _streamSubscriptions.add(
+      accelerometerEventStream(samplingPeriod: sensorInterval).listen(
+        (AccelerometerEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _accelerometerEvent = event;
+            if (_accelerometerUpdateTime != null) {
+              final interval = now.difference(_accelerometerUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _accelerometerLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _accelerometerUpdateTime = now;
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEventStream(samplingPeriod: sensorInterval).listen(
+        (GyroscopeEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _gyroscopeEvent = event;
+            if (_gyroscopeUpdateTime != null) {
+              final interval = now.difference(_gyroscopeUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _gyroscopeLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _gyroscopeUpdateTime = now;
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEventStream(samplingPeriod: sensorInterval).listen(
+        (MagnetometerEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _magnetometerEvent = event;
+            if (_magnetometerUpdateTime != null) {
+              final interval = now.difference(_magnetometerUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _magnetometerLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _magnetometerUpdateTime = now;
         },
         cancelOnError: true,
       ),
