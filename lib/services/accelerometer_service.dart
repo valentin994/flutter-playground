0dbb048873b93412cd8b33b9cfe7b47188ade45a
valentin994 <valentin.vareskic@gmail.com>
2024-02-28 15:24:48 +0100

    savepoint

--- lib/services/accelerometer_service.dart
+++ lib/services/accelerometer_service.dart
@@ -1,4 +1,41 @@
+import 'dart:async';
 
-class AccelerometerService {
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:flutter/material.dart';
 
+class AccelerometerService extends ChangeNotifier {
+  // ignore: unused_field
+  AccelerometerEvent? _accelerometerEvent;
+  DateTime? _accelerometerUpdateTime;
+  static const Duration _ignoreDuration = Duration(milliseconds: 30);
+  // ignore: unused_field
+  int? _accelerometerLastInterval;
+
+  AccelerometerService() {
+    _listenForAccelerometerEvents();
+  }
+
+  void _listenForAccelerometerEvents() async {
+    // Use StreamSubscription to manage subscription lifecycle
+    accelerometerEventStream(samplingPeriod: SensorInterval.normalInterval)
+        .listen(
+      (AccelerometerEvent event) {
+        final now = DateTime.now();
+        _accelerometerEvent = event;
+
+        // Check for update interval if previous update exists
+        if (_accelerometerUpdateTime != null) {
+          final interval = now.difference(_accelerometerUpdateTime!);
+          if (interval > _ignoreDuration) {
+            _accelerometerLastInterval = interval.inMilliseconds;
+          }
+        }
+        _accelerometerUpdateTime = now;
+      },
+    );
+  }
+
+  void printValues() {
+    print('The value: $_accelerometerEvent?.x');
+  }
 }
--- lib/ui/elevated_card.dart
+++ lib/ui/elevated_card.dart
@@ -1,61 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/services/accelerometer_service.dart';
 
-class ElevatedCard extends StatelessWidget {
+class ElevatedCard extends StatefulWidget {
   const ElevatedCard(
-      {Key? key,
+      {super.key,
       required this.title,
       required this.x,
       required this.y,
-      required this.z})
-      : super(key: key);
+      required this.z});
   final String title;
   final double x, y, z;
+
+  @override
+  State<ElevatedCard> createState() => _ElevatedCardState();
+}
+
+class _ElevatedCardState extends State<ElevatedCard> {
+  AccelerometerService serv = AccelerometerService();
+  void shittyFunc() {
+    serv.printValues();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 4,
+      clipBehavior: Clip.hardEdge,
       shadowColor: Colors.white,
-      child: SizedBox(
-        width: 300,
-        height: 100,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 8,
-            ),
-            Center(
-              child: Text(
-                title,
-                style: const TextStyle(color: Colors.black38),
-              ),
-            ),
-            const SizedBox(height: 12),
-            Row(
+      child: InkWell(
+          splashColor: Colors.white30.withAlpha(30),
+          onTap: () {
+                    shittyFunc();
+          },
+          child: SizedBox(
+            width: 300,
+            height: 100,
+            child: Column(
               children: [
-                Expanded(
-                    child: Center(
-                        child: Text(
-                  'x = $x',
-                  style: const TextStyle(fontSize: 18),
-                ))),
-                Expanded(
-                    child: Center(
-                        child: Text(
-                  'y = $y',
-                  style: const TextStyle(fontSize: 18),
-                ))),
-                Expanded(
-                    child: Center(
-                        child: Text(
-                  'z = $z',
-                  style: const TextStyle(fontSize: 18),
-                ))),
+                const SizedBox(
+                  height: 8,
+                ),
+                Center(
+                  child: Text(
+                    widget.title,
+                    style: const TextStyle(color: Colors.black38),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Center(
+                            child: Text(
+                      'x = ${widget.x}',
+                      style: const TextStyle(fontSize: 18),
+                    ))),
+                    Expanded(
+                        child: Center(
+                            child: Text(
+                      'y = ${widget.y}',
+                      style: const TextStyle(fontSize: 18),
+                    ))),
+                    Expanded(
+                        child: Center(
+                            child: Text(
+                      'z = ${widget.z}',
+                      style: const TextStyle(fontSize: 18),
+                    ))),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
-      ),
+            ),
+          )),
     );
   }
 }
